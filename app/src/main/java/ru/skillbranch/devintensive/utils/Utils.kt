4827735c55f47d9ba7edaeef7906870416ac9ad0
Igor Fursov <redectoo@gmail.com>
2019-07-02 02:28:12 +0300

    Utils redactor

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -21,152 +21,155 @@ object Utils {
     }
 
 
-    fun check_translite(variable: String?): String {
-        var stringBuilder = StringBuilder("")
-        variable?.forEach {
-            when (it) {
-                'а' -> stringBuilder.append("a")
 
-                'б' -> stringBuilder.append("b")
-
-                'в' -> stringBuilder.append("v")
-
-                'г' -> stringBuilder.append("g")
-
-                'д' -> stringBuilder.append("d")
-
-                'е' -> stringBuilder.append("e")
-
-                'ё' -> stringBuilder.append("e")
-
-                'ж' -> stringBuilder.append("zh")
-
-                'з' -> stringBuilder.append("z")
-
-                'и' -> stringBuilder.append("i")
-
-                'й' -> stringBuilder.append("i")
-
-                'к' -> stringBuilder.append("k")
-
-                'л' -> stringBuilder.append("l")
-
-                'м' -> stringBuilder.append("m")
-
-                'н' -> stringBuilder.append("n")
-
-                'о' -> stringBuilder.append("o")
-
-                'п' -> stringBuilder.append("p")
-
-                'р' -> stringBuilder.append("r")
-
-                'с' -> stringBuilder.append("s")
-
-                'т' -> stringBuilder.append("t")
-
-                'у' -> stringBuilder.append("u")
-
-                'ф' -> stringBuilder.append("f")
-
-                'х' -> stringBuilder.append("h")
-
-                'ц' -> stringBuilder.append("c")
-
-                'ч' -> stringBuilder.append("ch")
-
-                'ш' -> stringBuilder.append("sh")
-
-                'щ' -> stringBuilder.append("sh")
-
-                'ъ' -> stringBuilder.append("")
-
-                'ы' -> stringBuilder.append("i")
-
-                'ь' -> stringBuilder.append("")
-
-                'э' -> stringBuilder.append("e")
-
-                'ю' -> stringBuilder.append("yu")
-
-                'я' -> stringBuilder.append("ya")
-
-
-                'А' -> stringBuilder.append("A")
-
-                'Б' -> stringBuilder.append("B")
-
-                'В' -> stringBuilder.append("V")
-
-                'Г' -> stringBuilder.append("G")
-
-                'Д' -> stringBuilder.append("D")
-
-                'Е' -> stringBuilder.append("E")
-
-                'Ё' -> stringBuilder.append("E")
-
-                'Ж' -> stringBuilder.append("Zh")
-
-                'З' -> stringBuilder.append("Z")
-
-                'И' -> stringBuilder.append("I")
-
-                'Й' -> stringBuilder.append("I")
-
-                'К' -> stringBuilder.append("K")
-
-                'Л' -> stringBuilder.append("L")
-
-                'М' -> stringBuilder.append("M")
-
-                'Н' -> stringBuilder.append("N")
-
-                'О' -> stringBuilder.append("O")
-
-                'П' -> stringBuilder.append("P")
-
-                'Р' -> stringBuilder.append("R")
-
-                'С' -> stringBuilder.append("S")
-
-                'Т' -> stringBuilder.append("T")
-
-                'У' -> stringBuilder.append("U")
-
-                'Ф' -> stringBuilder.append("F")
-
-                'Х' -> stringBuilder.append("H")
-
-                'Ц' -> stringBuilder.append("C")
-
-                'Ч' -> stringBuilder.append("Ch")
-
-                'Ш' -> stringBuilder.append("Sh")
-
-                'Щ' -> stringBuilder.append("Sh")
-
-                'Ъ' -> stringBuilder.append("")
-
-                'Ы' -> stringBuilder.append("I")
-
-                'Ь' -> stringBuilder.append("")
-
-                'Э' -> stringBuilder.append("E")
-
-                'Ю' -> stringBuilder.append("Yu")
-
-                'Я' -> stringBuilder.append("Ya")
-                else -> stringBuilder.append(it)
-
-
-            }
-        }
-        return stringBuilder.toString()
-    }
 
 
     fun transliteration(payload: String, divider: String = " "): String {
+
+
+
+        fun check_translite(variable: String?): String {
+            var stringBuilder = StringBuilder("")
+            variable?.forEach {
+                when (it) {
+                    'а' -> stringBuilder.append("a")
+
+                    'б' -> stringBuilder.append("b")
+
+                    'в' -> stringBuilder.append("v")
+
+                    'г' -> stringBuilder.append("g")
+
+                    'д' -> stringBuilder.append("d")
+
+                    'е' -> stringBuilder.append("e")
+
+                    'ё' -> stringBuilder.append("e")
+
+                    'ж' -> stringBuilder.append("zh")
+
+                    'з' -> stringBuilder.append("z")
+
+                    'и' -> stringBuilder.append("i")
+
+                    'й' -> stringBuilder.append("i")
+
+                    'к' -> stringBuilder.append("k")
+
+                    'л' -> stringBuilder.append("l")
+
+                    'м' -> stringBuilder.append("m")
+
+                    'н' -> stringBuilder.append("n")
+
+                    'о' -> stringBuilder.append("o")
+
+                    'п' -> stringBuilder.append("p")
+
+                    'р' -> stringBuilder.append("r")
+
+                    'с' -> stringBuilder.append("s")
+
+                    'т' -> stringBuilder.append("t")
+
+                    'у' -> stringBuilder.append("u")
+
+                    'ф' -> stringBuilder.append("f")
+
+                    'х' -> stringBuilder.append("h")
+
+                    'ц' -> stringBuilder.append("c")
+
+                    'ч' -> stringBuilder.append("ch")
+
+                    'ш' -> stringBuilder.append("sh")
+
+                    'щ' -> stringBuilder.append("sh")
+
+                    'ъ' -> stringBuilder.append("")
+
+                    'ы' -> stringBuilder.append("i")
+
+                    'ь' -> stringBuilder.append("")
+
+                    'э' -> stringBuilder.append("e")
+
+                    'ю' -> stringBuilder.append("yu")
+
+                    'я' -> stringBuilder.append("ya")
+
+                    'А' -> stringBuilder.append("A")
+
+                    'Б' -> stringBuilder.append("B")
+
+                    'В' -> stringBuilder.append("V")
+
+                    'Г' -> stringBuilder.append("G")
+
+                    'Д' -> stringBuilder.append("D")
+
+                    'Е' -> stringBuilder.append("E")
+
+                    'Ё' -> stringBuilder.append("E")
+
+                    'Ж' -> stringBuilder.append("Zh")
+
+                    'З' -> stringBuilder.append("Z")
+
+                    'И' -> stringBuilder.append("I")
+
+                    'Й' -> stringBuilder.append("I")
+
+                    'К' -> stringBuilder.append("K")
+
+                    'Л' -> stringBuilder.append("L")
+
+                    'М' -> stringBuilder.append("M")
+
+                    'Н' -> stringBuilder.append("N")
+
+                    'О' -> stringBuilder.append("O")
+
+                    'П' -> stringBuilder.append("P")
+
+                    'Р' -> stringBuilder.append("R")
+
+                    'С' -> stringBuilder.append("S")
+
+                    'Т' -> stringBuilder.append("T")
+
+                    'У' -> stringBuilder.append("U")
+
+                    'Ф' -> stringBuilder.append("F")
+
+                    'Х' -> stringBuilder.append("H")
+
+                    'Ц' -> stringBuilder.append("C")
+
+                    'Ч' -> stringBuilder.append("Ch")
+
+                    'Ш' -> stringBuilder.append("Sh")
+
+                    'Щ' -> stringBuilder.append("Sh")
+
+                    'Ъ' -> stringBuilder.append("")
+
+                    'Ы' -> stringBuilder.append("I")
+
+                    'Ь' -> stringBuilder.append("")
+
+                    'Э' -> stringBuilder.append("E")
+
+                    'Ю' -> stringBuilder.append("Yu")
+
+                    'Я' -> stringBuilder.append("Ya")
+                    else -> stringBuilder.append(it)
+
+
+                }
+            }
+            return stringBuilder.toString()
+        }
 
 
         val (firstName, lastName) = parseFullName(payload)
